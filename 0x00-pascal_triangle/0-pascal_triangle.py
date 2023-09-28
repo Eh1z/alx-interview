488a8892322c8cfe6b5f7105c20cf8a171a9c1f7
Eh1z <godsentehiz11@gmail.com>
2023-09-28 22:32:28 +0000

    adjustments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,15 +5,15 @@
 
 
 def pascal_triangle(n):
-list = []
-if n <= 0:
-    return list
-else:
-    for i in range(n):
-        temp_list = []
-        for j in range(i + 1):
-          if j == 0 or j == i:
-            temp_list.append(1)
-          else:
-            temp_list.append(list[i-1][j-1] + list[i-1][j])
-        list.append(temp_list)
+    list = []
+    if n <= 0:
+        return list
+    else:
+        for i in range(n):
+            temp_list = []
+            for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list[i-1][j-1] + list[i-1][j])
+            list.append(temp_list)
