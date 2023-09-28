546d3fe65c5905d2675751f754bd01c3ec6d9f2f
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-09-28 23:26:22 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,8 +17,8 @@ for i in range(n):
 
 for i in range(n):
     for j in range(n-i-1):
-        print(" ", end="[")
+        print(" ", end="")
     for j in range(i+1):
-        print(list[i][j], end="]")
+        print(list[i][j], end=" ")
     print()
               
