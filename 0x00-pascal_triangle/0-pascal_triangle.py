3de82fc45c24cca168311d77a4d3f9a6751eb37b
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-09-28 23:21:44 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,5 +15,10 @@ for i in range(n):
         temp_list.append(list[i-1][j-1] + list[i-1][j])
     list.append(temp_list)
 print(list)
-
+for in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    for j in range(i+1):
+        print(list[i][j], end=" ")
+    print()
               
