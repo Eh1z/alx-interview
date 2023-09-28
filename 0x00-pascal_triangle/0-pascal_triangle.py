ef5f117088a326750064632004ed51d403990b33
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-09-28 23:15:02 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,10 +3,10 @@
 0-pascal_triangle.py
 """
 
-def pascal_triangle(n):
-  n = 4
-  list = []
-  for i in range (n):
+
+n = 4
+list = []
+for i in range(n):
     temp_list = []
     for j in range(i + 1):
       if j == 0 or j == i:
