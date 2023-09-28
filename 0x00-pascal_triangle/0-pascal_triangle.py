447c79c39d2a2636be68377ca8c662118d0bbe40
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-09-28 23:29:16 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,21 +4,16 @@
 """
 
 
-n = int(input("Enter a number: "))
+def pascal_triangle(n):
 list = []
-for i in range(n):
-    temp_list = []
-    for j in range(i + 1):
-      if j == 0 or j == i:
-        temp_list.append(1)
-      else:
-        temp_list.append(list[i-1][j-1] + list[i-1][j])
-    list.append(temp_list)
-
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ", end="")
-    for j in range(i+1):
-        print(list[i][j], end=" ")
-    print()
-              
+if n <= 0:
+    return list
+else:
+    for i in range(n):
+        temp_list = []
+        for j in range(i + 1):
+          if j == 0 or j == i:
+            temp_list.append(1)
+          else:
+            temp_list.append(list[i-1][j-1] + list[i-1][j])
+        list.append(temp_list)
