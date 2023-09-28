1d1432d582f724461b213feeed732b7475625fdf
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-09-28 23:50:04 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,17 +4,20 @@
 """
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-        
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
     matrix = []
-    for i in range(n):
-        rows = []
-
-        for j in range(i + 1):
-            result = comb(i, j)
-            rows.append(result)
-        matrix.append(rows)
-
-    # Return list of list
+    if n <= 0:
+        return matrix
+    matrix = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(matrix[i - 1]) - 1):
+            curr = matrix[i - 1]
+            temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
+        temp.append(1)
+        matrix.append(temp)
     return matrix
